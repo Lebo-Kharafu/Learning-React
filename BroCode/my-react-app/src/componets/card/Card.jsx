808a29db8a6dbd4f5324eaa6ff57061c773e60b6
Lebo-Kharafu <Kharafu23@gmail.com>
2025-07-09 22:16:03 +0200

    Added click Events on card

--- BroCode/my-react-app/src/componets/card/Card.jsx
+++ BroCode/my-react-app/src/componets/card/Card.jsx
@@ -1,15 +1,26 @@
 import profilePic from '../../assets/deadpool.png';
 import './Card.css';
 
-
 function Card() {
-    return(
+
+    var count = 0;
+
+    const handleClick = (e) => {
+        count++;
+        // console.log(e);
+        e.target.innerText ="❤️ "+count;
+    };
+
+    return (
         <article className="card">
-            <img className='card-img' src={profilePic} alt="My Profile Picture"></img>
-            <h2 className='card-title'>Lebo Kharafu</h2>
-            <p className='card-text'>BBD Busar and Wits Computer Science Student.</p>
+            <div className="img-container"  onClick={(e) => handleClick(e)}>
+                <img className="card-img" src={profilePic} alt="My Profile Picture" />
+                <span className="img-emoji">❤️ {count}</span>
+            </div>
+            <h2 className="card-title">Lebo Kharafu</h2>
+            <p className="card-text">BBD Busar and Wits Computer Science Student.</p>
         </article>
     );
 }
 
-export default Card
+export default Card;
