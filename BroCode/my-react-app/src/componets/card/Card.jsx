cb03a2bc9410003ce54f7b5225dc91a9a267e7b6
Lebo-Kharafu <Kharafu23@gmail.com>
2025-07-09 22:59:56 +0200

    Use updater function for next state using previous state

--- BroCode/my-react-app/src/componets/card/Card.jsx
+++ BroCode/my-react-app/src/componets/card/Card.jsx
@@ -11,7 +11,7 @@ function Card() {
         /*This is manual state management using event/ and dom*/
         /*likeCount++;
         e.target.innerText ="❤️ "+likeCount;*/
-        setCount(likeCount+1);
+        setCount(prevlikeCount => likeCount+1);
     };
 
     return (
