8fbffea3db9c7df8a65f88fecbf29a45702693f9
Lebo-Kharafu <Kharafu23@gmail.com>
2025-07-09 22:31:50 +0200

    did useState

--- BroCode/my-react-app/src/componets/card/Card.jsx
+++ BroCode/my-react-app/src/componets/card/Card.jsx
@@ -1,26 +1,31 @@
 import profilePic from '../../assets/deadpool.png';
+import React, {useState} from 'react';
 import './Card.css';
 
 function Card() {
 
-    var count = 0;
+    let  [likeCount,setCount] = useState(0);
 
+    
     const handleClick = (e) => {
-        count++;
-        // console.log(e);
-        e.target.innerText ="❤️ "+count;
+        /*This is manual state management using event/ and dom*/
+        /*likeCount++;
+        e.target.innerText ="❤️ "+likeCount;*/
+        setCount(likeCount+1);
     };
 
     return (
-        <article className="card">
-            <div className="img-container"  onClick={(e) => handleClick(e)}>
+        <article className="card" onClick={(e) => handleClick(e)}>
+            <div className="img-container" >
                 <img className="card-img" src={profilePic} alt="My Profile Picture" />
-                <span className="img-emoji">❤️ {count}</span>
+                <span className="img-emoji">❤️ {likeCount}</span>
             </div>
             <h2 className="card-title">Lebo Kharafu</h2>
             <p className="card-text">BBD Busar and Wits Computer Science Student.</p>
         </article>
     );
 }
+
+
 
 export default Card;
