c8e8bc7db6eef462f4ef2bad1ce2506c3ec70880
Lebo-Kharafu <Kharafu23@gmail.com>
2025-07-19 11:08:15 +0200

    Add realtime update, and Scrollview

--- Pedrotech/expo-app/app/(tabs)/index.tsx
+++ Pedrotech/expo-app/app/(tabs)/index.tsx
@@ -3,11 +3,9 @@ import { useAuth } from "@/lib/auth-context";
 import { Habit, RealtimeResponse } from "@/types/database.type";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { useEffect, useState } from "react";
-import {View, StyleSheet } from "react-native";
+import {View, StyleSheet, ScrollView } from "react-native";
 import { Query } from "react-native-appwrite";
 import { Text, Button, useTheme, Surface } from "react-native-paper";
-
-
 
 
 export default function Index() {
@@ -18,19 +16,33 @@ export default function Index() {
   const theme = useTheme();
 
   useEffect(() =>{
-    fetchHabits();
-    const HabitSub = client.subscribe(
-      `databases.${DATABASE_ID}.collections.${HABITS_COLLECTION_ID}.documents`,
-      (response:RealtimeResponse )=>{
-        return;
+    if(user){
+      const channel =  `databases.${DATABASE_ID}.collections.${HABITS_COLLECTION_ID}.documents`;
+      const HabitSub = client.subscribe(
+      channel,
+      (response:RealtimeResponse ) => {
+        if (response.events.includes("databases.*.collections.*.documents.*.create")) {
+          fetchHabits();
+        }
+        else if (response.events.includes("databases.*.collections.*.documents.*.update")) {
+          fetchHabits();
+        }
+        else if (response.events.includes("databases.*.collections.*.documents.*.delete")) {
+          fetchHabits();
+        }
       }
-    );
+      );
+
+      fetchHabits();
+      return () =>  {
+          HabitSub();
+        };
+    }
   },[user]);
 
 
   const fetchHabits = async () => {
     try {
-      
       const response = await databases.listDocuments(
         DATABASE_ID,
         HABITS_COLLECTION_ID,
@@ -56,48 +68,49 @@ export default function Index() {
         <Text variant="headlineSmall" style={styles.title}>
           {"Today's Habits"}
         </Text>
-        <Button mode="text" onPress={signOut} icon={"sign-out"} style={styles.button}>
+        <Button mode="text" onPress={signOut} icon={"logout"} style={styles.button}>
           {"Sign Out"}
         </Button>
       </View>
 
-      {
-        habits?.length === 0 ? (
-          <View style={styles.emptyState}>
-            <Text style={styles.emptyStateText}>
-              {"No Habit yet, Add Habits"}
-            </Text>
-          </View>
-        ):
-        (
-          habits?.map((habit,key) => (
-            <Surface style={styles.card} elevation={0}>              
-              <View key={key} style={styles.cardContent}>
-                <Text style={styles.cardTitle}>
-                  {habit.title}
-                </Text>
-                <Text style={styles.cardDesc}>
-                  {habit.description}
-                </Text>
-                <View style={styles.cardFooter}>
-                  <View style={styles.streakBadge}>
-                    <MaterialCommunityIcons name="fire" size={18} color={"#ff9800"} />
-                    <Text style={styles.streakText}>
-                      {habit.streak_count} day streak
-                    </Text>
-                  </View>
-                  <View style={styles.freqBadge}>
-                    <Text style={styles.freqText}>
-                      {habit.frequency.charAt(0).toUpperCase()+ habit.frequency.slice(1)}
-                    </Text>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {
+          habits?.length === 0 ? (
+            <View style={styles.emptyState}>
+              <Text style={styles.emptyStateText}>
+                {"No Habit yet, Add Habits"}
+              </Text>
+            </View>
+          ):
+          (
+            habits?.map((habit,key) => (
+              <Surface key={key} style={styles.card} elevation={0}>              
+                <View style={styles.cardContent}>
+                  <Text style={styles.cardTitle}>
+                    {habit.title}
+                  </Text>
+                  <Text style={styles.cardDesc}>
+                    {habit.description}
+                  </Text>
+                  <View style={styles.cardFooter}>
+                    <View style={styles.streakBadge}>
+                      <MaterialCommunityIcons name="fire" size={18} color={"#ff9800"} />
+                      <Text style={styles.streakText}>
+                        {habit.streak_count} day streak
+                      </Text>
+                    </View>
+                    <View style={styles.freqBadge}>
+                      <Text style={styles.freqText}>
+                        {habit.frequency.charAt(0).toUpperCase()+ habit.frequency.slice(1)}
+                      </Text>
+                    </View>
                   </View>
                 </View>
-              </View>
-            </Surface>
-          ))
-        )
-      }
-
+              </Surface>
+            ))
+          )
+        }
+      </ScrollView>
     </View>
   );
 }
